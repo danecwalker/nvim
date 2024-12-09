91b63704504edcc59b92c104b1656bae4c534d7c
Dane Walker <dane@danecwalker.com>
2024-12-10 09:40:19 +1000

    soft delete

--- lua/devdane/plugins/nvim-tree.lua
+++ lua/devdane/plugins/nvim-tree.lua
@@ -19,6 +19,13 @@ return {
           hide_dotfiles = false,
           hide_gitignored = true,
         },
+        commands = {
+          delete = function(state)
+            local path = state.tree:get_node().path
+            vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+            require("neo-tree.sources.manager").refresh(state.name)
+          end,
+        },
       },
     })
   end
