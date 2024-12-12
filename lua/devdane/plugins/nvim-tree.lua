a0538d28810b1bf894c8954c281ea0998a4325d8
DevDane <dane@danecwalker.com>
2024-12-12 10:26:36 +1000

    done

--- lua/devdane/plugins/nvim-tree.lua
+++ lua/devdane/plugins/nvim-tree.lua
@@ -1,32 +1,32 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  config = function()
-    require("neo-tree").setup({
-      close_if_last_window = true,
-      window = {
-        width = 40
-      },
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-        },
-        commands = {
-          delete = function(state)
-            local path = state.tree:get_node().path
-            vim.fn.system({ "trash", vim.fn.fnameescape(path) })
-            require("neo-tree.sources.manager").refresh(state.name)
-          end,
-        },
-      },
-    })
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			window = {
+				width = 40,
+			},
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+				commands = {
+					delete = function(state)
+						local path = state.tree:get_node().path
+						vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+						require("neo-tree.sources.manager").refresh(state.name)
+					end,
+				},
+			},
+		})
+	end,
 }
